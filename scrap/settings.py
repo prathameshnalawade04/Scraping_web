a62eb0aa0070b70e43881ee3322ea357f61c07a0
prathameshnalawade04 <prathames.nalwade04@gmail.com>
2026-02-10 07:22:15 +0530

    Allow all hosts for deployment

--- scrap/settings.py
+++ scrap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%v*^cucm)uvj219y56yhd2a@$u=^en_kf(!tr-($4ad=)=6xbk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='login'
